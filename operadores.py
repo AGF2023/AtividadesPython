93b8aad2c25c50a42898d4b9a140c11f259636b7
AGF2023 <132944423+AGF2023@users.noreply.github.com>
2023-05-22 14:37:33 -0400

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,22 @@
+numero = float(input('Digite um numero? '))
+
+resultado = numero // 2
+
+print(resultado)
+
+
+
+nome = input('Qual e o seu nome? ')
+print('prazer em te conhecer {:20}!'.format(nome))
+
+nome = input('Qual e o seu nome? ')
+print('prazer em te conhecer {:^20}!'.format(nome))
+
+nome = input('Qual e o seu nome? ')
+print('prazer em te conhecer {:50}!'.format(nome))
+
+nome = input('Qual e o seu nome? ')
+print('prazer em te conhecer {:^50}!'.format(nome))
+
+nome = input('Qual e o seu nome? ')
+print('prazer em te conhecer {:=^20}!'.format(nome))
---
+++ operadores02.py
@@ -0,0 +1,23 @@
+
+n1 = int(input('Digite um valor? '))
+n2 = int(input('Digite um outro valor? '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print('A soma e {}, o produto e {}, a divisao e {} '.format(s,m,d))
+print(' A divisao inteira e {}, e a potencia {}'.format(di, e) )
+
+
+n1 = float(input('Digite um valor? '))
+n2 = float(input('Digite um outro valor? '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print('A soma e {}, o produto e {}, a divisao e {} '.format(s,m,d))
+print(' A divisao inteira e {}, e a potencia {}'.format(di, e) )
