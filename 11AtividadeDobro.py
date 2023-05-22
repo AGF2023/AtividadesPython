21de92abcbbf5e8363235a5c139a37d4b1a9685a
AGF2023 <132944423+AGF2023@users.noreply.github.com>
2023-05-22 16:19:19 -0400

    Add files via upload

---
+++ 11AtividadeDobro.py
@@ -0,0 +1,5 @@
+metro = float(input('Digite um numero? '))
+centimetro = metro * 100
+milimetro = centimetro * 100
+print('a medida em centimetro e ', centimetro)
+print('A medida em milimetro e ', milimetro)
